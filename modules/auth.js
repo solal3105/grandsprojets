a22cfb8b00fe9a96aa92869c086e4ec3d5f9a2d3
lexskiies <alexis.cheyy@gmail.com>
2025-09-02 18:12:56 +0200

    Revert "Modifie les variables SUPABASE_ vers un fichier de config"
    This reverts commit e21f29e93b3bc3d90503e5d15759c29a70a5c774, reversing
    changes made to a548bb7dd33f071d68b4749a134251790387eb20.

--- modules/auth.js
+++ modules/auth.js
@@ -1,5 +1,4 @@
 // modules/auth.js
-import { SUPABASE_URL, SUPABASE_KEY } from '../config.js';
 ;(function(win){
   if (!win.supabase) {
     console.error('[AuthModule] Supabase CDN not loaded. Load @supabase/supabase-js before auth.js');
@@ -7,6 +6,8 @@ import { SUPABASE_URL, SUPABASE_KEY } from '../config.js';
   }
 
   // Same project as other modules
+  const SUPABASE_URL = 'https://wqqsuybmyqemhojsamgq.supabase.co';
+  const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxcXN1eWJteXFlbWhvanNhbWdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAxNDYzMDQsImV4cCI6MjA0NTcyMjMwNH0.OpsuMB9GfVip2BjlrERFA_CpCOLsjNGn-ifhqwiqLl0';
   const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
   const AuthModule = {
